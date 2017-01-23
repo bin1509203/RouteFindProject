33bc57350c2a20c1a590b784492249557618a57a
bin1509203 <bin1509203@naver.com>
2017-01-23 23:01:27 +0900

    CH4

---
+++ Header.js
@@ -0,0 +1,15 @@
+M:\React\react-tutorial\src\components
+λ touch Header.js
+
+import React from 'react';
+ 
+ 
+class Header extends React.Component {
+    render(){
+        return (
+            <h1>Header</h1>
+        );
+    }
+}
+ 
+export default Header;
