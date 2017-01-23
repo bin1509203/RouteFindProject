30cfb6ee6d0255b09131ef7f8fef65dd26307b0a
bin1509203 <bin1509203@naver.com>
2017-01-23 22:56:07 +0900

    CH4_1

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-
+ 
 class App extends React.Component {
-    sayHey() {
-        alert("hey");
-    }
-    render() {
-        let text = "Dev-Server";
-
-        let pStyle = {
-            color: 'aqua',
-            backgroundColor: 'black'
-        };
-
-        return ( 
+    render(){
+        return  (
             <div>
-            <h1 > Hello Velopert </h1>  
-            <h2 > Welcome to { text } </h2>
-            <button onClick = { this.sayHey } > Click Me </button>
-            <p style = { pStyle } > { 1 == 1 ? 'True' : 'False' } </p>  
-            </div >
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render(){
+        return (
+            <h1>Header</h1>
+        );
+    }
+}
+ 
+class Content extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Content</h2>
+                <p> Hey! </p>
+            </div>
         );
     }
 }
